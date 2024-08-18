73c680bb4bb238c30bf6d4db3232f7ef71453604
mati <mateuszeksli@o2.pl>
2024-08-19 01:04:02 +0200

    v0.1.3 wasted several hours back to ok system

--- units/player/arm.gd
+++ units/player/arm.gd
@@ -2,9 +2,9 @@ extends Line2D
 
 
 var start = 180.0
-@export var segments = 10
+@export var segments = 50
 @export var segment_size: float = 1.0
-@export var segment_length = 10
+@export var segment_length = 2
 
 
 func _ready():
@@ -15,32 +15,27 @@ func _ready():
 
 
 func _physics_process(delta):
-	points[0] = get_global_mouse_position()
+	points[get_point_count() - 1] = get_global_mouse_position()
 	for i in get_point_count() - 2:
-		points[i+1] = ((points[i+1] - points[i]).normalized() * segment_length) + points[i]
-	
-	points[get_point_count() - 1] = Vector2(100,start)
-	for i in get_point_count() - 1:
 		i = get_point_count() - i - 2
 		points[i] = ((points[i] - points[i + 1]).normalized() * segment_length) + points[i + 1]
 	
-	for i in get_point_count() - 2:
-		var angle01 = rad_to_deg(points[i].angle_to_point(points[i+1]))
-		var angle12 = rad_to_deg(points[i+2].angle_to_point(points[i+1]))
-		var odp
-		if angle01 * angle12 >= 0:
-			odp = abs(angle01) - abs(angle12)
-		else:
-			odp = abs(angle01) + abs(angle12)
-		odp = abs(odp)
+	points[0] = Vector2(100,start)
+	for i in get_point_count() - 1:
+		points[i+1] = ((points[i+1] - points[i]).normalized() * segment_length) + points[i]
+	
+	for i in get_point_count() - 1:
+		if points[i+1].y > 130:
+			points[i+1].y = 130
+	#never ever touch this shit, i was working on it and at the end after spending several hours it still doesn't work
+	#for i in get_point_count() - 9:
+		#var a1 = atan2(points[i].x - points[i+1].x, points[i].y - points[i+1].y)
+		#var a2 = atan2(points[i+2].x - points[i+1].x, points[i+2].y - points[i+1].y)
+		#var odp = rad_to_deg(a1 - a2)
+		#if odp < 0: odp += 360
 		#if odp < 90:
-			#points[i] = points[i+1] + Vector2(sin(deg_to_rad(90)),cos(deg_to_rad(90))) * segment_length
-	#for i in get_point_count() - 1:
-		#if points[i].angle_to_point(points[i+1]) > 90
-		#points[i+1] = ((points[i+1] - points[i]).normalized() * segment_length) + points[i]
-
-#function ConstrainDistance(point, anchor, distance) {
-  #return ((point - anchor).normalize() * distance) + anchor;
-#}
-func rotated_point(_center, _angle, _distance):
-	return _center + Vector2(sin(_angle),cos(_angle)) * _distance
+			#var diff = points[i+2] - points[i+1]
+			#points[i] = diff.rotated(deg_to_rad(-90)) + points[i+1]
+		#if odp > 270:
+			#var diff = points[i+2] - points[i+1]
+			#points[i] = diff.rotated(deg_to_rad(-90)) + points[i+1]
